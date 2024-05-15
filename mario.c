8d433dc0545f4cee902e181b59c2a81317fff466
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-05-15 09:30:32 +0000

    Wed, May 15, 2024, 6:30 AM -03:00

--- mario.c
+++ mario.c
@@ -1,11 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void print_row(int length)
+void print_row(int length);
 int main(void)
 {
     int height = get_int("Height: ");
-    print_row(height)
+    print_row(height);
 }
 
 void print_row(int length)
