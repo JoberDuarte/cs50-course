9a4fb617d8d0e1d7edef24a9daf27c08c3c51f2c
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-05-15 08:58:45 +0000

    Wed, May 15, 2024, 5:58 AM -03:00

--- hello.c
+++ hello.c
@@ -4,5 +4,8 @@
 int main(void)
 {
     string name = get_string("What is your name? ");
-    printf(" Hello, %s\n", name);
+    int age = get_int("What is your age? ");
+    string fone = get_string("what is your fone number? ");
+
+    printf(" Hello %s, Your age is %i and your fone number is %s\n", name, age, fone);
 }
