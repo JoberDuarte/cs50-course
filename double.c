531b47f9d8f3388b7bdade5bee7a32189416dbec
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-06-17 21:54:35 +0000

    Mon, Jun 17, 2024, 6:54 PM -03:00

---
+++ double.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <sdio.h>
+
+int main(void)
+{
+    int sequence [5];
+
+    sequence[0] = 1;
+    printf("%i\n", sequence[0]);
+
+    for (int i = 1; i < 5; i++)
+    {
+        
+    }
+}
