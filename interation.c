85d96539b6006bed46c836e2ba3561dd3101f5bf
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-07-03 08:38:59 +0000

    Wed, Jul 3, 2024, 5:38 AM -03:00

--- interation.c
+++ interation.c
@@ -1,4 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void draw(int n);
+
 int main(void)
+
+{
+    int
+}
+
+void draw(int n)
+
+{
+    draw(n-1)
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+    printf("\n")
+}
+
