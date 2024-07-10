c9497d17c5884934a4b555c7e3f23c8a1d4df43f
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-07-10 09:05:17 +0000

    Wed, Jul 10, 2024, 6:05 AM -03:00

--- addresses.c
+++ addresses.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
-#include <cs50.h>
 
 int main(void)
 {
-    string s = "HI!";
-    printf("%p\n", s);
-    printf("%p\n", &s[0]);
-    printf("%p\n", &s[1]);
-    printf("%p\n", &s[2]);
-    printf("%p\n", &s[3]);
+    char *s = "HI!";
+    printf("%s\n", s);
 }
