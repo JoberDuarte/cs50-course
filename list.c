67aebf3d73426ba1a4b5acabf8444812c6c40ada
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-07-18 08:19:53 +0000

    Thu, Jul 18, 2024, 5:19 AM -03:00

--- list.c
+++ list.c
@@ -1,16 +1,53 @@
+#include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+typedef struct node
 {
+    int number;
+    struct node *next;
+}
+node;
 
-    int list[3];
-    list[0] = 1;
-    list[1] = 2;
-    list[2] = 3;
+int main(int argc, char *argv[])
+{
+    // Memory for numbers
+    node *list = NULL;
 
- for (int i = 0; i < 3; i++)
+    // For each command-line argument
+    for (int i = 1; i < argc; i++)
     {
-        printf("%i\n", list[i]);
+        // Convert argument to int
+        int number = atoi(argv[i]);
+
+        // Allocate node for number
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            return 1;
+        }
+        n->number = number;
+        n->next = NULL;
+
+        // Prepend node to list
+        n->next = list;
+        list = n;
+    }
+
+    // Print numbers
+    node *ptr = list;
+    while (ptr != NULL)
+    {
+        printf("%i\n", ptr->number);
+        ptr = ptr->next;
+    }
+
+    // Free memory
+    ptr = list;
+    while (ptr != NULL)
+    {
+        node *next = ptr->next;
+        free(ptr);
+        ptr = next;
     }
 }
-
