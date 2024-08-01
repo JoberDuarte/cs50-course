ffa3d725f3fb97872daa2b6198d12c724364ce52
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-01 09:02:32 +0000

    Thu, Aug 1, 2024, 6:02 AM -03:00

--- world/hello.c
+++ world/hello.c
@@ -1,7 +1,11 @@
-#include <stdio.h>
+# include<cs50.h>
+# include <stdio.h>
 
 int main(void)
-
 {
-    printf("hello,word\n);
+    string name = get_string("What is your name? ");
+    int age = get_int("What is your age? ");
+    string fone = get_string("what is your fone number? ");
+
+    printf(" Hello %s, Your age is %i and your fone number is %s\n", name, age, fone);
 }
