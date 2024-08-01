6a76fe02dfab79856a6c83316bd850d404fd98a3
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-01 21:39:12 +0000

    Thu, Aug 1, 2024, 6:39 PM -03:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -17,13 +17,13 @@ int main(void)
     for (int i = 0; i < altura; i++)
     {
         //Mostre a linha de #
-        print_row( i + 1 , i + 1);
+        print_row();
     }
 }
 
 void print_row(int ponto, int tamanho)
 {
-    for (int i = 0; i < tamanho ; i--)
+    for (int i = 0; i < ponto ; i++)
     {
         printf(".");
     }
