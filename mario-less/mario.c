3aafa682002ed6a88a314d1e3ca815216f1f179c
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-02 11:51:47 +0000

    Fri, Aug 2, 2024, 8:51 AM -03:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -11,7 +11,7 @@ int main(void)
     {
         altura = get_int(" Height: ");
     }
-    while(altura < 1, altura < 9);
+    while(altura < 1 || altura > 8);
 
     for (int i = 0; i < altura; i++)
     {
