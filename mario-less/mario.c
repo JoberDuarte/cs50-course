248176bba1b28bfbfa6cdb55e4be574fbee211c9
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-02 11:47:56 +0000

    Fri, Aug 2, 2024, 8:47 AM -03:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -6,7 +6,6 @@ void print_row(int ponto, int tamanho);
 
 int main(void)
 {
-    // Pergunte ao usuario o tamanho da piramide
     int altura;
     do
     {
@@ -14,10 +13,8 @@ int main(void)
     }
     while(altura < 1);
 
-    //Mostre o tamanho da piramide
     for (int i = 0; i < altura; i++)
     {
-        //Mostre a linha de #
         print_row(i + 1 , altura);
     }
 }
