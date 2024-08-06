5c632ec88902e0c15705141d949cb6bdb3f22d18
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-06 22:45:13 +0000

    Tue, Aug 6, 2024, 7:45 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -5,19 +5,19 @@
 
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
-int compute_score(string player);
+int compute_score(string word);
 
 int main(void)
 {
-   string player1 = get_string("Player 1: ");
-   string player2 = get_string("Player 2: ");
+   string word1 = get_string("Player 1: ");
+   string word2 = get_string("Player 2: ");
 
    int score1 = compute_score(player1);
    int socre2 = compute_score(player2);
 
 
 }
-int compute_score(string player)
+int compute_score(string word)
 {
 
 }
