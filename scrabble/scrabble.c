9857b03bdbfb3554987968ebb75ed47ae037afe2
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-06 23:05:05 +0000

    Tue, Aug 6, 2024, 8:05 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -34,7 +34,7 @@ int compute_score(string word)
 {
     int score = 0;
 
-    for(int i = 0; len = strlen(word); i < len; i++)
+    for(int i = 0, len = strlen(word); i < len; i++)
     {
         if (isupper(word[i]))
         {
