bb37605eb4ef3bbec9c9f21995d5d40efab1a22d
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-06 22:37:01 +0000

    Tue, Aug 6, 2024, 7:37 PM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -8,5 +8,12 @@ int main(void)
    string player1 = get_string("Player 1: ");
    string player2 = get_string("Player 2: ");
 
+   int score1 = compute_score(player1);
+   int socre2 = compute_score(player2);
+
+
+}
+int compute_score(string player)
+{
 
 }
