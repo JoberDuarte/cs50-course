aa7059a8dec1950e6649df5254f010fd669ac316
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-07 22:45:39 +0000

    Wed, Aug 7, 2024, 7:45 PM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,4 +1,11 @@
-#include <ctype.h>
 #include <cs50.h>
 #include <stdio.h>
-#
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+
+int main(void)
+{
+    string text = get_string("TEXT: ");
+    
+}
