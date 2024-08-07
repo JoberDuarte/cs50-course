2b9cf04608a7c70439a0fa9eeb8be8fc689741b1
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-07 23:18:47 +0000

    Wed, Aug 7, 2024, 8:18 PM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,14 +5,17 @@
 #include <math.h>
 
 int count_letters(string text);
+int count_words(string text);
 
 int main(void)
 {
     string text = get_string("TEXT: ");
 
     int letter = count_letters(text);
+    int words = count_words(text);
 
-    printf("%i\n", letter);
+    printf("Letters : %i\n", letter);
+    printf("Words : %i\n", letter);
 
 
 }
@@ -34,6 +37,15 @@ int count_letters(string text)
 
 int count_words(string text)
 {
-    int words = 0;
-    
+    int words = 1;
+    int len  = strlen(text);
+
+    for(int i = 0; i < len; i++)
+    {
+        if(isspace(text[i]))
+        {
+            words += 1;
+        }
+    }
+    return words;
 }
