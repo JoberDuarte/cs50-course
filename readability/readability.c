a9a6be0b29c2013d2e65b94d6a88938b0f558fc0
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-07 22:55:34 +0000

    Wed, Aug 7, 2024, 7:55 PM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -4,22 +4,29 @@
 #include <string.h>
 #include <math.h>
 
+int count_letters(string text);
+
 int main(void)
 {
     string text = get_string("TEXT: ");
+
+    int letter = count_letters(text);
+
+    printf("%i\n", letter);
+
 
 }
 
 int count_letters(string text)
 {
-   int letter = 0
-   int len = strlen(text)
+   int letter = 0;
+   int len = strlen(text);
 
    for(i = 0; i < len; i++)
    {
     if(isalpha(text))
     {
-        
+        letter += 1
     }
    }
 }
