1afd77c719552e061312aee081ba70f8bb211b94
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-07 08:54:34 +0000

    Wed, Aug 7, 2024, 5:54 AM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -15,8 +15,6 @@ int main(void)
    int score1 = compute_score(word1);
    int score2 = compute_score(word2);
 
-   printf("%i\n",score1);
-
    if (score1 > score2)
    {
     printf("Player 1 wins!\n");
