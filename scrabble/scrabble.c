a05d53053edf89ed2f38a6920cc927f51dcee5a0
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-07 09:02:52 +0000

    Wed, Aug 7, 2024, 6:02 AM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,5 +1,5 @@
-#include <ctype.h>
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -9,33 +9,31 @@ int compute_score(string word);
 
 int main(void)
 {
-   string word1 = get_string("Player 1: ");
-   string word2 = get_string("Player 2: ");
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
 
-   int score1 = compute_score(word1);
-   int score2 = compute_score(word2);
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
 
-   if (score1 > score2)
-   {
-    printf("Player 1 wins!\n");
-   }
-   else if (score2 > score1)
-   {
-    printf("Player 2 wins!\n");
-   }
-   else
-   {
-    printf("Tie!\n");
-   }
-
-
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score2 > score1)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
 }
 int compute_score(string word)
 {
     int score = 0;
     int len = strlen(word);
 
-    for(int i = 0; i < len; i++)
+    for (int i = 0; i < len; i++)
     {
         if (isupper(word[i]))
         {
@@ -45,8 +43,6 @@ int compute_score(string word)
         {
             score += POINTS[word[i] - 'a'];
         }
-
     }
     return score;
 }
-
