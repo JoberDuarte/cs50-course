46cb7556809e62b008956544a70fd632d10f572c
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-12 21:59:30 +0000

    Mon, Aug 12, 2024, 6:59 PM -03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -127,7 +127,7 @@ int main(int argc, string argv[])
 bool vote(int voter, int rank, string name)
 {
     // TODO
-    for(int i = )
+    for(int i = 0; i < voter_count; i ++)
     return false;
 }
 
