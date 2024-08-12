7e13b0a820431d63ee1e2738bafe776cdb556002
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-12 22:58:41 +0000

    Mon, Aug 12, 2024, 7:58 PM -03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -131,7 +131,7 @@ bool vote(int voter, int rank, string name)
     {
         for(int j = 0; j < candidade_count; j++)
         {
-            if(strcmp(name, candidates[i].name))
+            if(strcmp(name, candidates[i].name == 0))
             {
                 voter[i][j] == candidates[i].name;
                 return true;
