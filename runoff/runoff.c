c16c5591ce80ce514e69f7035a0c658d42fed066
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-13 22:00:08 +0000

    Tue, Aug 13, 2024, 7:00 PM -03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -150,7 +150,7 @@ void tabulate(void)
     {
         for(int j = 0; j < candidate_count; j++)
         {
-            if(candidates[preferences[i][j]].eliminate == false )
+            if(candidates[preferences[i][j]].eliminated == false )
             {
                 candidates[preferences[i][j]].votes++;
                 break;
