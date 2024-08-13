64e6e6ad47cb5b6fd3074a8c7749772847d4597b
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-13 12:51:48 +0000

    Tue, Aug 13, 2024, 9:51 AM -03:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -134,7 +134,7 @@ bool vote(int voter, int rank, string name)
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            if (strcmp(name, candidates[i].name) == 0)
+            if (strcmp(name, candidates[j].name) == 0)
             {
                 preferences[i][j] = i;
                 return true;
