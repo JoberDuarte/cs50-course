537fa0ce25e31b78478faf3885a34017cfc050b3
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-15 12:48:08 +0000

    Thu, Aug 15, 2024, 9:48 AM -03:00

--- volume/volume.c
+++ volume/volume.c
@@ -41,6 +41,8 @@ int main(int argc, char *argv[])
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
 
+    fread(&buffer, sizeof(int16_t), 1, input)
+
     // Close files
     fclose(input);
     fclose(output);
