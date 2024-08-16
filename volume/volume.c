ca4c5670995ecc70f342296a8a20b51e535e591e
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-16 16:00:38 +0000

    Fri, Aug 16, 2024, 1:00 PM -03:00

--- volume/volume.c
+++ volume/volume.c
@@ -45,7 +45,7 @@ int main(int argc, char *argv[])
 
     buffer *= factor;
 
-    fwrite(&buffer, sizeof(int16_t), 1, output)
+    fwrite(&buffer, sizeof(int16_t), 1, output);
 
     // Close files
     fclose(input);
