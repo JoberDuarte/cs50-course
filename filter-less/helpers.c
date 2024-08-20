7b83f9ec07c9b087c00541422b50540b15700e58
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-20 11:31:30 +0000

    Tue, Aug 20, 2024, 8:31 AM -03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -19,6 +19,17 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for(int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            uint8_t red = (uint8_t) (0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 0.189 *image[i][j].rgbtBlue);
+            uint8_t green = (uint8_t) (0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen + 0.168 *image[i][j].rgbtBlue);
+            uint8_t blue = (uint8_t) (0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 *image[i][j].rgbtBlue);
+            image[i][j].rgbtRed = red;
+            image[i][j].rgbtGreen = green;
+            image[i][j].rgbtBlue = blue;
+        }
     return;
 }
 
