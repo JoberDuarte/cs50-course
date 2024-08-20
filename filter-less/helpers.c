f358e9478a38f6a74bdcd56f193c257db1e932eb
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-20 11:11:30 +0000

    Tue, Aug 20, 2024, 8:11 AM -03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,19 +3,11 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    uint8_t buffer_r;
-    uint8_t buffer_g;
-    uint8_t buffer_b;
     for(int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            fread(&buffer_r, 1, 1, image[i][j].rgbtRed);
-            fread(&buffer_g, 1, 1, image[i][j].rgbtGreen);
-            fread(&buffer_b, 1, 1, image[i][j].rgbtBlue);
-            fwrite((&buffer_r * 0.3), 1, 1, image[i][j].rgbRed);
-            fwrite((&buffer_g * 0.59), 1, 1, image[i][j].rgbtGreen);
-            fwrite((&buffer_b * 0.11), 1, 1, image[i][j].rgbtBlue);
+            uint8_t media =  (image[i][j].rgbtRed);
 
         }
     }
