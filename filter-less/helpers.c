f76f420a686502fa2c50ff10bf63f03a721dff5e
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-21 09:39:49 +0000

    Wed, Aug 21, 2024, 6:39 AM -03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -65,11 +65,21 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE copy[height][width];
+    sum_red = 0;
+    sum_green = 0;
+    sum_blue = 0;
+    pixels_count = 0;
     for (int i = 0; i < height; i++)
     {
         for(int j = 0; j < width; j++)
         {
+            copy[i][j].rgbtRed = image[i][j].rgbtRed;
+            copy[i][j].rgbtGreen = image[i][j].rgbtGreen;
+            copy[i][j].rgbtBlue = image[i][j].rgbtBlue;
+
             
+
         }
     }
     return;
