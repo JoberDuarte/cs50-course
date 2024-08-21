c6fccba151f95489ff0c438ea3bff7f76c84ca73
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-21 09:05:30 +0000

    Wed, Aug 21, 2024, 6:05 AM -03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -47,7 +47,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < (width / 2); j++)
         {
 
-            image[i][j].rgbtRed = image[i][ width - j ].rgbtRed && 
+            image[i][j].rgbtRed = image[i][ width - j ].rgbtRed && image[i][ width - j ].rgbtRed = image[i][j].rgbtRed;
 
             image[i][j].rgbtGreen = image[i][ width - j ].rgbtGreen;
             image[i][j].rgbtBlue = image[i][ width - j ].rgbtBlue;
