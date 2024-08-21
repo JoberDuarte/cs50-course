fbe81f9bd8fe1c9e398088dc4b68d8b0f8a321bd
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-21 08:48:48 +0000

    Wed, Aug 21, 2024, 5:48 AM -03:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -42,6 +42,17 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for(int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            uint8_t media = (uint8_t) ((image[i][j].rgbtRed + image[i][j].rgbtGreen +image[i][j].rgbtBlue )/ 3);
+            image[i][j].rgbtRed = media;
+            image[i][j].rgbtGreen = media;
+            image[i][j].rgbtBlue = media;
+        }
+    }
+    return;
     return;
 }
 
