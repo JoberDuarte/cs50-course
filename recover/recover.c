9b95f4be7dec78f3f7fa733059f9f9c068099daf
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-23 10:03:57 +0000

    Fri, Aug 23, 2024, 7:03 AM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -24,7 +24,8 @@ int main(int argc, char *argv[])
  {
     if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0)== 0xe0)
     {
-     FILE *image = 000.jpg
+     FILE *image = 000.jpg;
+     sprintf(image,"%03i.jpg", 2);
 
 
     }
