c0fa6135b53633d4b308ced4f6198e550f602d5a
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-23 21:54:15 +0000

    Fri, Aug 23, 2024, 6:54 PM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 
    FILE *image = NULL;
 
-while (fread(&buffer, 1, 512, card) == 512)
+while (fread(buffer, 1, 512, card) == 512)
 {
 
     if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && ((buffer[3] & 0xf0)== 0xe0))
