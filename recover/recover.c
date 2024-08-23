321280d1538a351067e502a2d8ddc0a1355ce181
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-23 10:44:56 +0000

    Fri, Aug 23, 2024, 7:44 AM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
      char *filename;
      sprintf(filename,"%03i.jpg",count_image );
      count_image++;
-     FILE *image = fopen(image, "w");
+     FILE *image = fopen(filename, "w");
 
     while(buffer[0] != 0xff && buffer[1] != 0xd8 && buffer[2] != 0xff && (buffer[3] & 0xf0) != 0xe0)
       {
@@ -40,6 +40,6 @@ int main(int argc, char *argv[])
 
     }
  }
- fclose(image)
+ fclose(image);
 
 }
