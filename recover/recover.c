30c5ddfd834123f7f2d9cfb14fd7222118d3e274
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-23 09:38:10 +0000

    Fri, Aug 23, 2024, 6:38 AM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 
  while (fread(&buffer, 1, 512, card) == 512)
  {
-    
+    if(buffer[0] == 0xff, buffer[1] == 0xd8,buffer[2] == 0xff,(buffer[0] == 0xff) )
  }
 
 
