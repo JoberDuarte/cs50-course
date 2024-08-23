0a41c308b541da5ce610431ccae83f139f972f19
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-23 10:22:51 +0000

    Fri, Aug 23, 2024, 7:22 AM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -5,20 +5,20 @@
 
 int main(int argc, char *argv[])
 {
-   if  (argc != 2)
-   {
-      printf("Usage: ./recover FILE\n");
-      return 1;
-   }
+    if (argc != 2)
+    {
+        printf("Usage: ./recover FILE\n");
+        return 1;
+    }
 
-   FILE *card = fopen(argv[1], "r");
-   if (card == NULL)
-   {
-      printf("Could not open this card\n");
-      return 1;
-   }
+    FILE *card = fopen(argv[1], "r");
+    if (card == NULL)
+    {
+        printf("Could not open this card\n");
+        return 1;
+    }
 
-   uint8_t buffer[512];
+    uint8_t buffer[512];
 
  while (fread(&buffer, 1, 512, card) == 512)
  {
