4f7101c4309bccfd15415d1e4bb87caf08f54af2
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-23 10:39:10 +0000

    Fri, Aug 23, 2024, 7:39 AM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -25,8 +25,8 @@ int main(int argc, char *argv[])
  {
     if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0)== 0xe0)
     {
-     char *image = 000;
-     sprintf(image,"%03i.jpg", 2);
+     char *filename;
+     sprintf(filename,"%03i.jpg", );
      FILE *image = fopen(image, "w");
 
     while(buffer[0] != 0xff && buffer[1] != 0xd8 && buffer[2] != 0xff && (buffer[3] & 0xf0) != 0xe0)
