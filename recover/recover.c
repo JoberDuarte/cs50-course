8bbaa770397c4a1c0eb064149483e1969227e4b5
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-23 21:23:06 +0000

    Fri, Aug 23, 2024, 6:23 PM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -25,8 +25,9 @@ int main(int argc, char *argv[])
 
  while (fread(&buffer, 1, 512, card) == 512)
  {
+   FILE *image = NULL;
+   
     if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && ((buffer[3] & 0xf0)== 0xe0))
-     FILE *image = fopen(filename, "w");
 
       {
          if(image != NULL)
