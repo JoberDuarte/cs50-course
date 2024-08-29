13c31f434ccd5dc0ea7562e9edb48ec8c8e34397
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-29 09:54:41 +0000

    Thu, Aug 29, 2024, 6:54 AM -03:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -54,12 +54,11 @@ unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
     // I had help from duck here
-    int total = 0;
+    unsigned long total = 0;
 
-    for(int i = 0; i < 3; i++)
-    if(word[i]!= '\0')
+    for(int i = 0; i < strlen(word); i++)
     {
-        total += toupper(word[i]);
+        total += tolower(word[i]);
     }
     return total % N;
 
