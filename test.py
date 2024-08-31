d45b34910022c2f2a29fd0473db1de188cdb7019
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-31 16:59:04 +0000

    Sat, Aug 31, 2024, 1:59 PM -03:00

--- test.py
+++ test.py
@@ -1,21 +1,20 @@
-while True:
-    try:
-        n = int(input("Height: "))
-        if 1 <= n <= 8:
-            break
-        else:
-            print("Please Enter an integer from 1 to 8")
-    except ValueError:
-        print("Invalid input NOT INTEGER")
+def get_height():
+    while True:
+        try:
+            n = int(input("Height: "))
+            if 1 <= n <= 8:
+                return n
+            else:
+                print("Please enter an integer from 1 to 8.")
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
 
-for i in range(1, n + 1):
-    for k in range(n - i):
-        print(" ", end="")
-    for j in range(i):
-        print("#", end="")
-    print(end =" ")
-    for p in range(i):
-        print("#", end="")
-    print()
+def print_pyramid(height):
+    for i in range(1, height + 1):
+        # Print spaces and hashes for the left side
+        print(" " * (height - i) + "#" * i, end="  ")
+        # Print hashes for the right side
+        print("#" * i)
 
-
+height = get_height()
+print_pyramid(height)
