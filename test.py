8a36446aafc76dc3b4bdd54947afc71af083639e
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-31 17:11:25 +0000

    Sat, Aug 31, 2024, 2:11 PM -03:00

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 def get_height():
+    # Input validation
     while True:
         try:
             n = int(input("Height: "))
@@ -10,6 +11,7 @@ def get_height():
             print("Invalid input. Please enter an integer.")
 
 def print_pyramid(height):
+    #
     for i in range(1, height + 1):
         # Print spaces and hashes for the left side
         print(" " * (height - i) + "#" * i, end="  ")
@@ -17,5 +19,6 @@ def print_pyramid(height):
         print("#" * i)
 
 height = get_height()
+
 print_pyramid(height)
 
