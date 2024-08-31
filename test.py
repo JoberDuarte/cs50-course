f2780db0eac14a22053b20e1c0985993dca39b87
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-08-31 16:13:30 +0000

    Sat, Aug 31, 2024, 1:13 PM -03:00

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ n = int(input("Height: "))
 while n < 1 or n > 8:
     n = int(input("Height: "))
 
-for i in range(n + 1):
+for i in range(1, n + 1):
     for k in range(n - i):
         print(" ", end="")
     for j in range(i):
