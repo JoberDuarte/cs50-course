9b3c84f5e13d578ef59d37624964433b5446a47d
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-02 22:44:53 +0000

    Mon, Sep 2, 2024, 7:44 PM -03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,6 +1,16 @@
 text = input("TEXT: ")
 
 def count_letters(text):
-    
+    letters = sum(1 for n in text if n.isalpha())
+    print(f"{letters}")
+
+
+
+count_letters(text)
+
+words = len(text.split())
+
+print(f"{words}")
+
 
 
