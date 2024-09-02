c33e97b6c0f60ccdbf71ed6532af8ce4aa837e17
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-02 22:26:14 +0000

    Mon, Sep 2, 2024, 7:26 PM -03:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,6 +2,7 @@ from itertools import tee, islice
 
 card_number = input("Number: ")
 
+
 def split_list(card_number):
     it_1, it_2 = tee(card_number)
     return list(map(int, islice(it_1, 0, None, 2))), list(map(int, islice(it_2, 1, None, 2)))
@@ -12,7 +13,7 @@ def multiply_list(multiply_list):
     for i in range(len(multiply_list)):
         multiply_list[i] = multiply_list[i] * 2
 
-    #separates each digit from the list
+    # separates each digit from the list
     separeted_digits = []
     for number in multiply_list:
         number_str = str(number)
@@ -21,8 +22,9 @@ def multiply_list(multiply_list):
 
     return separeted_digits
 
+
 def credit_card(number):
-    if len(number) == 15 and number[0] == "3"  and number[1] in ["4", "7"]:
+    if len(number) == 15 and number[0] == "3" and number[1] in ["4", "7"]:
         print("AMEX")
     elif len(number) == 16 and number[0] == "5" and number[1] in ["1", "2", "3", "4", "5"]:
         print("MASTERCARD")
@@ -36,9 +38,9 @@ list_1, list_2 = split_list(card_number)
 
 
 if len(card_number) % 2 == 0:
-   separeted_digits = multiply_list(list_1)
-   sum_list_1 = sum(separeted_digits)
-   sum_list_2 = sum(list_2)
+    separeted_digits = multiply_list(list_1)
+    sum_list_1 = sum(separeted_digits)
+    sum_list_2 = sum(list_2)
 
 elif len(card_number) % 2 != 0:
     separeted_digits = multiply_list(list_2)
