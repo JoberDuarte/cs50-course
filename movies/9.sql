483ed7ada6b8ddfb2febcc8602c98b3bb252fe6b
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-08 17:39:06 +0000

    Sun, Sep 8, 2024, 2:39 PM -03:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,13 +1,12 @@
-SELECT DISTINCT UPPER(TRIM(people.name)) AS normalized_name
+SELECT DISTINCT people.name
 FROM people
 WHERE id IN (
-  SELECT person_id
-  FROM stars
-  WHERE movie_id IN (
-    SELECT id
-    FROM movies
-    WHERE year = 2004
-  )
+    SELECT person_id
+    FROM stars
+    WHERE movie_id IN (
+        SELECT id
+        FROM movies
+        WHERE year = 2004
+    )
 )
-ORDER BY  
- normalized_name ASC;
+ORDER BY people.birth ASC;
