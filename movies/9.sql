aedfead008d5386f2450eb605728942f5a214c76
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-08 17:45:45 +0000

    Sun, Sep 8, 2024, 2:45 PM -03:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,12 +1,5 @@
-SELECT DISTINCT people.name
-FROM people
-WHERE id IN (
-    SELECT person_id
-    FROM stars
-    WHERE movie_id IN (
-        SELECT id
-        FROM movies
-        WHERE year = 2004
-    )
-)
+SELECT DISTINCT name
+JOIN stars ON stars.person_id = people.id
+JOIN movies ON movies.id = stars.movie_id
+WHERE movies.years = 2004
 ORDER BY people.birth ASC;
