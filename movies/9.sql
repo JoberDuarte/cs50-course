2d00dd5f569408ec07c93a532524796cb61ccbf6
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-08 17:20:37 +0000

    Sun, Sep 8, 2024, 2:20 PM -03:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,3 +1,15 @@
+SELECT id, TRIM(name) AS name
+FROM people
+WHERE TRIM(name) = 'Craig T. Nelson';
+
+
+SELECT TRIM(p.name) AS name
+FROM people p
+JOIN stars s ON p.id = s.person_id
+JOIN movies m ON s.movie_id = m.id
+WHERE m.year = 2004 AND TRIM(p.name) = 'Craig T. Nelson';
+
+
 SELECT DISTINCT TRIM(name) AS name
 FROM people
 WHERE id IN
