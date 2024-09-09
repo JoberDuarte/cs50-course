c329290050d9fc77713f79df0866613394cb06c8
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-09 21:06:33 +0000

    Mon, Sep 9, 2024, 6:06 PM -03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -48,4 +48,10 @@ WHERE license_plate IN
 
    );
 
-
+SELECT *
+FROM atm_transactions
+WHERE year = 2023
+AND month = 7
+AND day = 28
+AND atm_location = 'Leggett Street'
+AND transaction_type = 'deposit';
