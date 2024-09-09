a32b556b67fec2111a524e6c5e04be377537dbea
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-09 08:47:36 +0000

    Mon, Sep 9, 2024, 5:47 AM -03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,8 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+SELECT description
+FROM crime_scene_reports
+WHERE year = 2023
+AND month = 7
+AND day = 28
+AND street = 'Humphrey Street';
