f9a6789240b02aa6eb79682b3396dc4ec22ad48c
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-09 20:57:15 +0000

    Mon, Sep 9, 2024, 5:57 PM -03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -24,18 +24,7 @@ AND day = 28;
 
 
 -- Selecionando os dados de todas as pessoas que deixaram a padaria ate 20 minutos depois do roubo
-SELECT * FROM people
-WHERE license_plate IN
-(
-    SELECT license_plate
-    FROM bakery_security_logs
-    WHERE year = 2023
-    AND month = 7
-    AND day = 28
-    AND hour = 10
-    AND minute BETWEEN 16 AND 35
-);
-
+-- e fizeram uma ligacao com duracao de menos de 1 minuto
 
 SELECT * FROM people
 WHERE license_plate IN
