d4c98796d1d4b4e1659b3054500c9be17c7db1b9
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-09 21:40:31 +0000

    Mon, Sep 9, 2024, 6:40 PM -03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -65,4 +65,17 @@ WHERE license_plate IN
     )
     );
 
-SELECT * FROM people
+SELECT *
+FROM people
+WHERE phone_number IN
+(
+        SELECT receiver
+        FROM phone_calls
+        WHERE year = 2023
+        AND month = 7
+        AND day = 28
+        AND duration < 60
+        AND caller = '(286) 555-6063'
+        AND caller = '(770) 555-1861'
+        AND caller = '(367) 555-5533'
+);
