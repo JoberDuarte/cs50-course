39b48e1d3c35cf1d62247e37dcab38f0f8554626
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-09 22:47:01 +0000

    Mon, Sep 9, 2024, 7:47 PM -03:00

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ FROM passengers
 JOIN flights ON flights.id = passengers.flight_id
 WHERE (passport_number = 1988161715
 OR passport_number = 3592750733
+OR passport_number = 5773159633
 OR passport_number = 2438825627
-OR passport_number = 3391710505
 OR passport_number = 3592750733)
 
 AND origin_airport_id = 8;
