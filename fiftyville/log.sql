41562d1cca555f78292c28368a2e211506db44b2
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-09 21:54:21 +0000

    Mon, Sep 9, 2024, 6:54 PM -03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -65,17 +65,30 @@ WHERE license_plate IN
     )
     );
 
+-- verifica as pessoas que receberam as ligacoes das pessoas que atendem os criterios acima
 SELECT *
 FROM people
 WHERE phone_number IN
 (
-        SELECT receiver
-        FROM phone_calls
-        WHERE year = 2023
-        AND month = 7
-        AND day = 28
-        AND duration < 60
-        AND (caller = '(286) 555-6063'
-        OR caller = '(770) 555-1861'
-        OR caller = '(367) 555-5533')
+    SELECT receiver
+    FROM phone_calls
+    WHERE year = 2023
+    AND month = 7
+    AND day = 28
+    AND duration < 60
+    AND (caller = '(286) 555-6063'
+    OR caller = '(770) 555-1861'
+    OR caller = '(367) 555-5533')
 );
+
+-- verifica os pares
+
+SELECT *
+FROM phone_calls
+WHERE year = 2023
+AND month = 7
+AND day = 28
+AND duration < 60
+AND (caller = '(286) 555-6063'
+OR caller = '(770) 555-1861'
+OR caller = '(367) 555-5533');
