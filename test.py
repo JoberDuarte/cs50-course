b62f29f8d148136ea2b10713141fccac7bf9d537
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-09 21:44:52 +0000

    Mon, Sep 9, 2024, 6:44 PM -03:00

--- test.py
+++ test.py
@@ -1,44 +1,12 @@
-SELECT * FROM people
-WHERE license_plate IN
-(
-    SELECT license_plate
-    FROM bakery_security_logs
-    WHERE year = 2023
-    AND month = 7
-    AND day = 28
-    AND hour = 10
-    AND minute BETWEEN 16 AND 35
-)
-    AND phone_number IN
-    (
-        SELECT caller
+SELECT *
         FROM phone_calls
         WHERE year = 2023
         AND month = 7
         AND day = 28
         AND duration < 60
-
-   )
-
-
-AND id IN
-(
-    SELECT person_id
-    FROM bank_accounts
-    WHERE account_number IN
-    (
-        SELECT account_number
-        FROM atm_transactions
-        WHERE year = 2023
-        AND month = 7
-        AND day = 28
-        AND atm_location = 'Leggett Street'
-        AND transaction_type = 'withdraw'
-
-    )
-);
-
-
+        AND caller = '(286) 555-6063'
+        OR caller = '(770) 555-1861'
+        OR caller = '(367) 555-5533'
 
 
 
