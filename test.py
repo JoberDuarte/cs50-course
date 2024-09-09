949684d737e22232f9941cd378753fa65a9f10a1
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-09 21:50:48 +0000

    Mon, Sep 9, 2024, 6:50 PM -03:00

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ SELECT *
         AND month = 7
         AND day = 28
         AND duration < 60
-        AND caller = ('(286) 555-6063'
+        AND (caller = '(286) 555-6063'
         OR caller = '(770) 555-1861'
         OR caller = '(367) 555-5533');
 
