305fcdd999607a25a1dda88a08043ff7e46ce007
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-09 21:47:12 +0000

    Mon, Sep 9, 2024, 6:47 PM -03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -69,13 +69,13 @@ SELECT *
 FROM people
 WHERE phone_number IN
 (
-        SELECT receiver
+        SELECT *
         FROM phone_calls
         WHERE year = 2023
         AND month = 7
         AND day = 28
         AND duration < 60
-        AND caller = '(286) 555-6063'
+        AND caller = ('(286) 555-6063'
         OR caller = '(770) 555-1861'
-        OR caller = '(367) 555-5533'
+        OR caller = '(367) 555-5533')
 );
--- test.py
+++ test.py
@@ -4,9 +4,9 @@ SELECT *
         AND month = 7
         AND day = 28
         AND duration < 60
-        AND caller = '(286) 555-6063'
+        AND caller = ('(286) 555-6063'
         OR caller = '(770) 555-1861'
-        OR caller = '(367) 555-5533'
+        OR caller = '(367) 555-5533');
 
 
 
