e0753ea9dd7e3600bdffa4cd21db056461ca86e5
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-10 08:08:18 +0000

    Tue, Sep 10, 2024, 5:08 AM -03:00

--- test.py
+++ test.py
@@ -1,19 +1,1 @@
-SELECT *
-FROM passengers
-JOIN flights ON flights.id = passengers.flight_id
-WHERE (passport_number = 1988161715
-OR passport_number = 3592750733
-OR passport_number = 5773159633
-OR passport_number = 2438825627
-OR passport_number = 3592750733)
-
-AND origin_airport_id = 8;
-
-
-SELECT * FROM flights
-WHERE origin_airport_id = 8
-AND year = 2023
-AND month = 7
-AND day = 29
-ORDER BY hour;
 
---
+++ test.sql
@@ -0,0 +1,6 @@
+SELECT * FROM people
+JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
+WHERE year = 2023
+AND month = 7
+AND day = 28
+AND hour = 10;
