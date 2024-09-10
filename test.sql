1f964ee5f8636f3bf6e8d6a8f853234ebcef994b
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-10 08:27:29 +0000

    Tue, Sep 10, 2024, 5:27 AM -03:00

--- test.sql
+++ test.sql
@@ -1,8 +1,8 @@
 SELECT * FROM people
 JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
-JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
 WHERE year = 2023
 AND month = 7
 AND day = 28
 AND hour = 10
-AND activity = 'exit';
+AND activity = 'exit'
+
