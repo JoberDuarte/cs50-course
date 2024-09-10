9fb177dd8a5c45cac36056956bd516c1299d4d96
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-10 08:23:51 +0000

    Tue, Sep 10, 2024, 5:23 AM -03:00

--- test.sql
+++ test.sql
@@ -1,5 +1,6 @@
 SELECT * FROM people
 JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
+JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
 WHERE year = 2023
 AND month = 7
 AND day = 28
