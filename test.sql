b6fd2cb9affe28320b732e27408a0fc1c3084a9c
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-10 08:47:20 +0000

    Tue, Sep 10, 2024, 5:47 AM -03:00

--- test.sql
+++ test.sql
@@ -1,10 +1,16 @@
-SELECT * FROM people
+-- Selecionando os dados de todas as pessoas que deixaram a padaria ate 20 minutos depois do roubo
+-- e fizeram uma ligacao com duracao de menos de 1 minuto
+
+SELECT *, bank_accountsFROM people
 JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
 JOIN phone_calls ON phone_calls.caller = people.phone_number
+JOIN bank_accounts ON bank_accounts.person_id = people.id
 WHERE bakery_security_logs.year = 2023
 AND bakery_security_logs.month = 7
 AND bakery_security_logs.day = 28
 AND bakery_security_logs.hour = 10
 AND bakery_security_logs.activity = 'exit'
 AND phone_calls.duration < 60;
+
+
 
