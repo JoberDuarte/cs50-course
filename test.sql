b24c658c8dd68b1012edcaf47f50719d17a6f0c4
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-10 08:48:56 +0000

    Tue, Sep 10, 2024, 5:48 AM -03:00

--- test.sql
+++ test.sql
@@ -1,7 +1,7 @@
 -- Selecionando os dados de todas as pessoas que deixaram a padaria ate 20 minutos depois do roubo
 -- e fizeram uma ligacao com duracao de menos de 1 minuto
 
-SELECT *, bank_accounts.account_number FROM people
+SELECT * FROM people
 JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
 JOIN phone_calls ON phone_calls.caller = people.phone_number
 JOIN bank_accounts ON bank_accounts.person_id = people.id
