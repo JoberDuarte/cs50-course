b20e1a36817b6bbd595a1a3d092fdbd280caf412
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-10 08:19:28 +0000

    Tue, Sep 10, 2024, 5:19 AM -03:00

--- test.sql
+++ test.sql
@@ -3,4 +3,5 @@ JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license
 WHERE year = 2023
 AND month = 7
 AND day = 28
-AND hour BETWEEN 9 AND 11;
+AND hour = 10
+AND activity = 'exit';
