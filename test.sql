e8b74ff897a47028775da5b5d5fe184a1ce403df
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-10 08:32:23 +0000

    Tue, Sep 10, 2024, 5:32 AM -03:00

--- test.sql
+++ test.sql
@@ -1,9 +1,10 @@
 SELECT * FROM people
 JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
 JOIN phone_calls ON phone_calls.caller = people.phone_number
-WHERE year = 2023
-AND month = 7
-AND day = 28
-AND hour = 10
-AND activity = 'exit'
+WHERE bakery_security_logs.year = 2023
+AND bakery_security_logs.month = 7
+AND bakery_security_logs.day = 28
+AND bakery_security_logs.hour = 10
+AND bakery_security_logs.activity = 'exit'
+AND phone_calls.duration < 60;
 
