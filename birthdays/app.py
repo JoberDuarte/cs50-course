19b5358d49fcd5e486262b4536fc27bdfd7ae210
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-22 21:22:21 +0000

    Sun, Sep 22, 2024, 6:22 PM -03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -31,7 +31,7 @@ def index():
         month = request.form.get("month")
         day = request.form.get("day")
 
-        db.execute("INSERT INTO birthdaays (name, month, day) VALUES(?, ?, ?)" name, month, day)
+        db.execute("INSERT INTO birthdaays (name, month, day) VALUES(?, ?, ?)" name, month, day);
 
         return redirect("/")
 
