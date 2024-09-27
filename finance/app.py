689ddd83dfc9649deff03672f1d21e03dadcbbf4
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-09-27 08:16:53 +0000

    Fri, Sep 27, 2024, 5:16 AM -03:00

--- finance/app.py
+++ finance/app.py
@@ -132,11 +132,11 @@ def register():
 
                # Check if register_password == confirm_password
         elif request.form.get("register_password") == request.form.get("confirm_password"):
-            password_hash = generate_password_hash("resister_password")
+            password_hash = generate_password_hash(request.form.get("register_password"))
             username = request.form.get("register_username")
 
             try:
-                db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", (username, password_hash,))
+                db.execute("INSERT INTO users (username, hash, cash) VALUES (?, ?, ?)", (username, password_hash, 10000.00))
 
             except ValueError:
                 return apology("User already exists, change username")
