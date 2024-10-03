dd627ee80e3c6043e15e19db1171b59f33c42c34
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-10-03 16:08:03 +0000

    Thu, Oct 3, 2024, 1:08 PM -03:00

--- exercicios-github/and_gate.py
+++ exercicios-github/and_gate.py
@@ -1,2 +1,19 @@
 """
-An AND Gate is a logic boolean algebra which results 
+An AND Gate is a logic boolean algebra which results to 1 (True) if both the
+inputs are 1, and 0 (False) otherwise.
+
+Following is the truth table of an AND Gate:
+    ------------------------------
+    | Input 1 | Input 2 | Output |
+    ------------------------------
+    |    0    |    0    |    0   |
+    |    0    |    1    |    0   |
+    |    1    |    0    |    0   |
+    |    1    |    1    |    1   |
+    ------------------------------
+
+Refer - https://www.geeksforgeeks.org/logic-gates-in-python/
+"""
+
+
+def and_gate
