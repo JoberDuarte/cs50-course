7621344ff0690a4e32e759686678d05f7c05d1f5
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-10-03 17:40:45 +0000

    Thu, Oct 3, 2024, 2:40 PM -03:00

--- exercicios-github/sudoku_solver.py
+++ exercicios-github/sudoku_solver.py
@@ -20,5 +20,6 @@ squares = cross(rows, cols)
 unitlist = (
     [cross(rows, c) for c in cols]
     +[cross(r, cols) for r in rows]
-    +[]
+    +[cross(rs, cs) for rs in ("ABC" , "DEF", "GHI") for cs in ("123", "456", "789")]
 )
+
