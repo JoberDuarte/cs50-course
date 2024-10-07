cce61e10ab7bfef553daa9ebde27f635c9decfea
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-10-07 23:13:22 +0000

    Mon, Oct 7, 2024, 8:13 PM -03:00

--- finance/app.py
+++ finance/app.py
@@ -61,7 +61,11 @@ def buy():
         if cash < total_shares_cost:
             return apology("not enough cash")
 
-        #
+        #update users table
+        db.execute("UPDATE users SET cash = cash - :total_shares_cost WHERE id = :user_id"; total_shares_cost=total_shares_cost, user_id=session["user_id"])
+
+        #insert purchase on transaction table
+        db.execute("INSERT INTO transactions ()")
 
 
 
