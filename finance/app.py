d37951b6936a70e86a111f8105d6ae356fd0c3c7
JoberDuarte <161221925+JoberDuarte@users.noreply.github.com>
2024-10-11 11:33:17 +0000

    Fri, Oct 11, 2024, 8:33 AM -03:00

--- finance/app.py
+++ finance/app.py
@@ -303,7 +303,7 @@ def funds():
         if not funds or not funds.isdigit() or int(funds) <= 0:
             return apology("Must provide a positive number")
 
-        cash = db.execute("SELECT cash FROM users WHERE id = :user_id", user_id=session["user_id"])[0]["cash"]
+        # cash = db.execute("SELECT cash FROM users WHERE id = :user_id", user_id=session["user_id"])[0]["cash"]
         db.execute("UPDATE users SET cash = cash + funds WHERE id = :user_id", funds=funds, user_id=session["user_id"])
 
 
